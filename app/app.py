02bf71e3d21926f4287aebc5ed327ad47107cbb4
Azeez Tiamiyu <hayzed@MacBook-Air-6.local>
2026-06-03 19:51:38 +0100

    defined health and index endpoints for flask api

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy", "service": "deloitte-demo"}), 200
+
+@app.route("/")
+def index():
+    return jsonify({"message": "CI/CD pipeline is live"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
